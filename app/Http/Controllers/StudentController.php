dfdf05f2957828ab1ba54f0dd0a977f790a7c557
GonzaloAizaga <129554571+GonzaloAizaga@users.noreply.github.com>
2024-04-17 20:29:36 -0300

    laravel

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+use App\Http\Requests\StoreStudentRequest;
+use App\Http\Requests\UpdateStudentRequest;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('students.index', [
+            'students' => Student::latest()->paginate(3)
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('students.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreStudentRequest $request) : RedirectResponse
+    {
+        Student::create($request->all());
+        return redirect()->route('students.index')
+                ->withSuccess('El alumno se agrego correctamente.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Student $student) : View
+    {
+        return view('students.show', [
+            'student' => $student
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Student $student) : View
+    {
+        return view('students.edit', [
+            'students' => $student
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateStudentRequest $request, Student $student) : RedirectResponse
+    {
+        $student->update($request->all());
+        return redirect()->back()
+                ->withSuccess('El estudiante se modifico correctamente.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Student $student) : RedirectResponse
+    {
+        $student->delete();
+        return redirect()->route('students.index')
+                ->withSuccess('El usuario se elimino correctamente');
+    }
+}
